b9dd925a65b54bc9613fd51ccad47e72a0152538
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-10-26 18:29:17 -0600

    Added stripe payment intent func and helpers

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/IM-Deane/virtual-terminal
 go 1.18
 
 require github.com/go-chi/chi/v5 v5.0.7
+
+require github.com/stripe/stripe-go/v73 v73.14.0
---
+++ internal/card/card.go
@@ -0,0 +1,74 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v73"
+	"github.com/stripe/stripe-go/v73/paymentintent"
+)
+
+type Card struct {
+	Secret string
+	Key string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusID int
+	Amount int
+	Currency string
+	LastFour string
+	BankReturnCode string
+}
+
+// Charge alias for CreatePaymentIntent with a more meaningful name
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
+}
+
+// CreatePaymentIntent handles stripe payment
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	// create payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount: stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	// NOTE: can add additional props by: params.AddMetadata("key", "value")
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeError, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeError.Code)
+		}
+		return nil, msg, err
+	}
+	return pi, "", nil
+}
+
+// cardErrorMessage helper for rendering stripe error messages
+func cardErrorMessage(code stripe.ErrorCode) string {
+	var msg =""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired"
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Incorrect CVC code"
+	case stripe.ErrorCodeIncorrectZip:
+		msg = "Incorrect zip/postal code"
+	case stripe.ErrorCodeAmountTooLarge:
+		msg = "The amount is too large to charge to your card"
+	case stripe.ErrorCodeAmountTooSmall:
+		msg = "The amount is too small to charge to your card"
+	case stripe.ErrorCodeBalanceInsufficient:
+		msg = "Insufficient balance"
+	case stripe.ErrorCodePostalCodeInvalid:
+		msg = "Your postal code is invalid"
+	default:
+		msg = "Your card was declined"
+	}
+	return msg
+}
