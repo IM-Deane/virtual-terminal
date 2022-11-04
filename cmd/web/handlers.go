254bd6491ef77c0ab28d4d1a8e330b911b93ca7d
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-11-03 20:04:56 -0600

    Created subscription receipt page and api

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -311,3 +311,10 @@ func (app *application) BronzePlan(w http.ResponseWriter, r *http.Request) {
 		app.errorLog.Println(err)
 	}
 }
+
+// BronzePlanReceipt renders the bronze plan recipet page
+func (app *application) BronzePlanReceipt(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "receipt-plan", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
